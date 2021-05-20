cc9b5e4e44b2ef9546ffbbfd85af0fa751f0edb2
sweng <shiqiang-tommy.weng@nokia-sbell.com>
2021-05-20 11:15:01 +0800

    Modify switch

---
+++ 5G/switch.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+FILE="/home/`whoami`/script/5G/config.sh"
+
+switch_component()
+{
+    component=`sed -n '4p' ${FILE} | awk -F '=' '{print $2}' | tr -dc 'A-Z0-9'`
+
+    if [ "${component}" == "L2LO" ]; then
+        sed -i 's/COMPONENT="L2LO"/COMPONENT="L2PS"/g' ${FILE}
+    else
+        sed -i 's/COMPONENT="L2PS"/COMPONENT="L2LO"/g' ${FILE}
+    fi
+
+    current_component=`sed -n '4p' ${FILE} | awk -F '=' '{print $2}' | tr -dc 'A-Z0-9'`
+    echo "Switch to component ${current_component}"
+}
+
+switch_dbglog()
+{
+    flag=`sed -n '7p' ${FILE} | awk -F '=' '{print $2}' | tr -dc 'a-z'`
+
+    if [ "${flag}" == "off" ]; then
+        sed -i 's/DBGLOG=off/DBGLOG=on/g' ${FILE}
+    else
+        sed -i 's/DBGLOG=on/DBGLOG=off/g' ${FILE}
+    fi
+
+    current_flag=`sed -n '4p' ${FILE} | awk -F '=' '{print $2}' | tr -dc 'a-z'`
+    echo "Switch DBG log to ${current_flag}"
+}
+
+show_prompt()
+{
+    echo "Use option:"
+    echo "    component"
+    echo "    dbg"
+}
+
+case "$1" in
+    "component")
+        switch_component
+        ;;
+    "dbg")
+        switch_dbglog
+        ;;
+    *)
+        show_prompt
+        ;;
+esac
