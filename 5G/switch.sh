c7fd215bdd882fca9da7dd446277e16e14c18740
sweng <shiqiang-tommy.weng@nokia-sbell.com>
2021-05-20 14:22:38 +0800

    update switch

--- 5G/switch.sh
+++ 5G/switch.sh
@@ -4,7 +4,7 @@ FILE="/home/`whoami`/script/5G/config.sh"
 
 switch_component()
 {
-    component=`sed -n '4p' ${FILE} | awk -F '=' '{print $2}' | tr -dc 'A-Z0-9'`
+    component=`sed -n '4p' ${FILE} | awk -F'=' '{print $2}' | tr -dc 'A-Z0-9'`
 
     if [ "${component}" == "L2LO" ]; then
         sed -i 's/COMPONENT="L2LO"/COMPONENT="L2PS"/g' ${FILE}
@@ -12,13 +12,13 @@ switch_component()
         sed -i 's/COMPONENT="L2PS"/COMPONENT="L2LO"/g' ${FILE}
     fi
 
-    current_component=`sed -n '4p' ${FILE} | awk -F '=' '{print $2}' | tr -dc 'A-Z0-9'`
+    current_component=`sed -n '4p' ${FILE} | awk -F'=' '{print $2}' | tr -dc 'A-Z0-9'`
     echo "Switch to component ${current_component}"
 }
 
 switch_dbglog()
 {
-    flag=`sed -n '7p' ${FILE} | awk -F '=' '{print $2}' | tr -dc 'a-z'`
+    flag=`sed -n '7p' ${FILE} | awk -F'=' '{print $2}' | tr -dc 'a-z'`
 
     if [ "${flag}" == "off" ]; then
         sed -i 's/DBGLOG=off/DBGLOG=on/g' ${FILE}
@@ -26,8 +26,14 @@ switch_dbglog()
         sed -i 's/DBGLOG=on/DBGLOG=off/g' ${FILE}
     fi
 
-    current_flag=`sed -n '4p' ${FILE} | awk -F '=' '{print $2}' | tr -dc 'a-z'`
+    current_flag=`sed -n '7p' ${FILE} | awk -F'=' '{print $2}' | tr -dc 'a-z'`
     echo "Switch DBG log to ${current_flag}"
+}
+
+show()
+{
+    gawk 'NR == 4 || NR == 7' ${FILE}  # gawk: gnu awk
+    #sed -n '4p;7p' ${FILE}
 }
 
 show_prompt()
@@ -35,6 +41,7 @@ show_prompt()
     echo "Use option:"
     echo "    component"
     echo "    dbg"
+    echo "    show"
 }
 
 case "$1" in
@@ -43,6 +50,9 @@ case "$1" in
         ;;
     "dbg")
         switch_dbglog
+        ;;
+    "show")
+        show
         ;;
     *)
         show_prompt
